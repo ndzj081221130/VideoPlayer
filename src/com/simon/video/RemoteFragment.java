1ccf5f10b2c4673e43520a8f9a31aee5e4514e79
ndzj981479673@gmail.com <ndzj981479673@gmail.com>
2013-09-09 12:43:41 +0800

    update autocomplete

--- src/com/simon/video/RemoteFragment.java
+++ src/com/simon/video/RemoteFragment.java
@@ -21,9 +21,10 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class RemoteFragment extends Fragment {
 	private List<String> myMusicList = new ArrayList<String>();
- 	private AutoCompleteTextView autoTv;
- 	private Button search ;
- 	private ListView listview;
+	private AutoCompleteTextView autoTv;
+	private Button search;
+	private ListView listview;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -34,22 +35,23 @@ public class RemoteFragment extends Fragment {
 	private View inflateAndSetupView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState, int layoutResourceId) {
 		View layout = inflater.inflate(layoutResourceId, container, false);
-		autoTv = (AutoCompleteTextView) layout.findViewById(R.id.autoCompleteTextView);
+		autoTv = (AutoCompleteTextView) layout
+				.findViewById(R.id.autoCompleteTextView);
 		search = (Button) layout.findViewById(R.id.button1);
 		listview = (ListView) layout.findViewById(R.id.local_list);
-		search.setOnClickListener(new OnClickListener() {			
+		search.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				playMusic();
 			}
 		});
 
-		initAutoComplete("history",autoTv);
+		initAutoComplete("history", autoTv);
 		getMusicList();
 		return layout;
 	}
-	
-	void getMusicList() {		 		
+
+	void getMusicList() {
 		ArrayAdapter<String> musicList = new ArrayAdapter<String>(
 				getActivity(), R.layout.music_item, myMusicList);
 		listview.setAdapter(musicList);
@@ -60,24 +62,31 @@ public class RemoteFragment extends Fragment {
 				playMusic();
 			}
 		});
- 	}
-	
+	}
+
 	/**
-	 * 初始化AutoCompleteTextView，最多显示5项提示，使
-	 * AutoCompleteTextView在一开始获得焦点时自动提示
-	 * @param field 保存在sharedPreference中的字段名
-	 * @param auto 要操作的AutoCompleteTextView
+	 * 初始化AutoCompleteTextView，最多显示5项提示，使 AutoCompleteTextView在一开始获得焦点时自动提示
+	 * 
+	 * @param field
+	 *            保存在sharedPreference中的字段名
+	 * @param auto
+	 *            要操作的AutoCompleteTextView
 	 */
-	private void initAutoComplete(String field,AutoCompleteTextView auto) {
-		SharedPreferences sp = getActivity().getSharedPreferences("video_url", 0);
-		String longhistory = sp.getString("history", getString(R.string.no_records));
-		String[]  hisArrays = longhistory.split(",");
- 		for(String str:hisArrays)
-			myMusicList.add(str);
+	private void initAutoComplete(String field, AutoCompleteTextView auto) {
+		SharedPreferences sp = getActivity().getSharedPreferences("video_url",
+				0);
+		String longhistory = sp.getString("history",
+				getString(R.string.no_records));
+		String[] hisArrays = longhistory.split(",");
+//		hisArrays.
+		for (String str : hisArrays) {
+			if (str.equals("nothing") == false)
+				myMusicList.add(str);
+		}
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_dropdown_item_1line, hisArrays);
-		//只保留最近的50条的记录
-		if(hisArrays.length > 50){
+				android.R.layout.simple_dropdown_item_1line, myMusicList);
+		// 只保留最近的50条的记录
+		if (hisArrays.length > 50) {
 			String[] newArrays = new String[50];
 			System.arraycopy(hisArrays, 0, newArrays, 0, 50);
 			adapter = new ArrayAdapter<String>(getActivity(),
@@ -92,18 +101,17 @@ public class RemoteFragment extends Fragment {
 			public void onFocusChange(View v, boolean hasFocus) {
 				AutoCompleteTextView view = (AutoCompleteTextView) v;
 				if (hasFocus) {
-						view.showDropDown();
+					view.showDropDown();
 				}
 			}
 		});
-		
+
 	}
- 
-	 
+
 	void playMusic() {
- 		Intent intent = new Intent(getActivity(),VideoActivity.class);
- 		intent.putExtra("path", autoTv.getText().toString().trim());
+		Intent intent = new Intent(getActivity(), VideoActivity.class);
+		intent.putExtra("path", autoTv.getText().toString().trim());
 		intent.putExtra("type", "remote");
 		startActivity(intent);
-	} 
+	}
 }
